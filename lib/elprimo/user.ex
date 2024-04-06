a2cb8c210208e8a01962857174c7f6516616bdb9
semenInRussia <hrams205@gmail.com>
2024-04-06 18:23:28 +0300

    scheme for user

---
+++ lib/elprimo/user.ex
@@ -0,0 +1,23 @@
+defmodule Elprimo.User do
+  @moduledoc """
+  A schema for Elprimo.User for sync with Database.
+  """
+
+  use Ecto.Schema
+  import Ecto.Query
+
+  schema "user" do
+    field(:username, :string, default: nil)
+    field(:telegram, :integer)
+    field(:admin, :boolean)
+  end
+
+  def by_telegram_id(id) do
+    query = from(u in __MODULE__, where: u.telegram == ^id, select: u)
+    Elprimo.Repo.one(query)
+  end
+
+  def from_tgx(%Telegex.Type.User{username: username, id: id}) do
+    %__MODULE__{username: username, telegram: id, admin: false}
+  end
+end
