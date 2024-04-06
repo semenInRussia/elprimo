c6e30021806453299b92d55ad339fcc93577f5f5
semenInRussia <hrams205@gmail.com>
2024-04-06 21:23:08 +0300

    rich /question command with 2 steps!!

--- lib/elprimo/handlers/question_handler.ex
+++ lib/elprimo/handlers/question_handler.ex
@@ -2,18 +2,17 @@ defmodule Elprimo.Handlers.QuestionHandler do
   use Telegex.Chain, :message
   alias Elprimo.Question
   alias Elprimo.User
+  alias Elprimo.State
   alias Telegex.Type.Message
   import Elprimo.Utils
 
-  @command "/question"
+  @command "question"
 
   require Logger
 
   @impl Telegex.Chain
   def match?(msg, _ctx) when not is_nil(msg.text) do
-    msg.text
-    |> String.trim()
-    |> String.contains?(@command)
+    check_command(msg.text, @command) or State.check(msg.from.id, :question)
   end
 
   def match?(_msg, _ctx) do
@@ -21,16 +20,24 @@ defmodule Elprimo.Handlers.QuestionHandler do
   end
 
   @impl Telegex.Chain
-  def handle(%Message{from: user, text: txt} = msg, context) do
-    Logger.warning(msg)
+  def handle(%Message{from: user} = msg, context) do
+    handle_state(State.get(user.id), msg, context)
+  end
 
+  def handle_state(state, %Message{from: user, text: txt} = msg, ctx) do
     u = User.by_telegram_id(user.id)
-    txt = txt |> String.replace(@command, "") |> String.trim()
 
-    Elprimo.Repo.insert(%Question{time: now(), from: u.id, text: txt, isquery: false})
+    case state do
+      :question ->
+        Elprimo.Repo.insert(%Question{time: now(), from: u.id, text: txt, isquery: false})
+        Telegex.send_message(msg.chat.id, "Спасибо за то, что задали вопрос!")
+        State.update(user.id, :none)
 
-    Telegex.send_message(msg.chat.id, "Спасибо за то, что задали вопрос!")
+      :none ->
+        Telegex.send_message(msg.chat.id, "Ваш вопрос (как можно конкретнее)?")
+        State.update(user.id, :question)
+    end
 
-    {:done, context}
+    {:done, ctx}
   end
 end
